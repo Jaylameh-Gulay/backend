e1e776c1d4d8bd7536f6c27870370feebf7d64e3
Jaylameh-Gulay <jaylamehgulay318@gmail.com>
2025-07-14 21:12:09 +0800

    Added User Controller functionality (index, show, store and destroy)

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+         return User::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($validated['password']);
+        $user = User::create($validated);
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+           return User::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+   
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+     /**
+     * Update the email to get the specified resource in storage.
+     */
+    public function email(Request $request, string $id)
+    {
+        //
+    }
+
+     /**
+     * Update the password to get the specified resource in storage.
+     */
+    public function password(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //Hard delete - bali deleted jud siya sa database.
+        $user = User::findOrFail($id);
+        $user->delete();
+        return $user;
+    }
+}
